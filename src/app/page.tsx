56b7e593341b37327ae832ec2123a863517af5c5
JorgeSarricolea <jjorgesarricolea18@gmail.com>
2023-07-04 19:32:55 -0600

    fix UI of HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-interface Movie {
-  id: number;
-  title: string;
-  poster_path: string;
-  release_date: string;
-  vote_average: number;
-  genre_ids: number[];
-}
+import React from 'react';
+import 'tailwindcss/tailwind.css';
+import { Movie } from '@/types/Movie';
 
+// Fetching API
 const API_KEY = '4f298a53e552283bee957836a529baec';
 
 async function fetchMovies(): Promise<Movie[]> {
@@ -15,29 +11,32 @@ async function fetchMovies(): Promise<Movie[]> {
   return data.results;
 }
 
-async function Home() {
+const Home: React.FC = async () => {
   const movies: Movie[] = await fetchMovies();
-  console.log(movies);
+  // console.log(movies);
+
   return (
     <div>
-      <h1>¡Sí funciona!</h1>
-      <div>
-        <ul>
-          {movies.map((movie) => (
-            <li key={movie.id}>
-              <div>
-                <h5>{movie.title}</h5>
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                <p>{movie.release_date}</p>
-                <p>{movie.vote_average}</p>
-                <p>{movie.genre_ids}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
+      <div className="grid grid-cols-5 gap-4 py-10 px-10">
+        {movies.map((movie) => (
+          <div
+            key={movie.id}
+            className="bg-gray-200 p-4 hover:bg-blue-200 transform hover:scale-105 transition duration-300 rounded-2xl cursor-pointer"
+          >
+            <h5 className="text-lg font-bold mb-2">{movie.title}</h5>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              className="w-full h-auto mb-2 rounded-2xl"
+            />
+            <p className="text-sm">Release Date: {movie.release_date}</p>
+            <p className="text-sm">Vote Average: {movie.vote_average}</p>
+            <p className="text-sm">Genre IDs: {movie.genre_ids.join(', ')}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default Home;
